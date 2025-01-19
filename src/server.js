8330fc34db98fb5dce5ea783736bafc7a5ad7c49
Gamein <gamein.official.mail@gmail.com>
2025-01-19 12:27:43 +0900

    bug fix

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const port = process.env.PORT || 3000;
+
+// 静的ファイルをpublicディレクトリから提供
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ルートへのGETリクエストでindex.htmlを返す
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const { generateRoomId, generateDeck, dealCards } = require('./gameLogic');
 const { addPlayer, getPlayersInRoom, removePlayer } = require('./players');
