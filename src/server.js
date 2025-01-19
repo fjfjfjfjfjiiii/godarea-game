27e92edbe36b0c6e9249ec9721b00e2ab6c3537c
Gamein <gamein.official.mail@gmail.com>
2025-01-19 23:07:40 +0900

    change server.js again

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Current __dirname:', __dirname);
+const fs = require('fs');
+const path = require('path');
+
+// /opt/render/project の構造をログに出力
+const projectPath = '/opt/render/project/';
+fs.readdir(projectPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+        console.error('Error reading project directory:', err);
+    } else {
+        console.log('Project directory structure:');
+        files.forEach(file => console.log(file.name));
+    }
+});
